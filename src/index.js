20c1723775b09f8a312487d17750b81c275b3063
Juan Manuel GR <jmgr198810@gmail.com>
2025-11-22 02:12:56 +0000

    Se crea el servidor de la aplicación

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { PORT } from "./config.js";
+import userRoutes from "./routes/users.route.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(userRoutes);
+
 app.listen(PORT);
 
 console.log("Server on port", PORT);
