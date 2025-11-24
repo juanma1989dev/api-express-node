75975e06265fe0399a75160cf7ab2af01b4fb476
Juan Manuel GR <jmgr198810@gmail.com>
2025-11-24 20:33:56 +0000

    Se simplifican las rutas y se aggregan calidaciones a los datos de entrada

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,55 +1,18 @@
 import { Router } from "express";
-import { pool } from "../db.js";
+import userController from "../controllers/users.controller.js";
+import { validateFields } from "../middlewares/validator.middleware.js";
+import { userDTO } from "../dtos/user-dto.js";
 
 const router = Router();
 
-router.get("/users", async (req, res) => {
-  const { rows } = await pool.query("select * from users");
-  res.json(rows);
-});
+router.get("/users", userController.getUsers);
 
-router.get("/users/:id", async (req, res) => {
-  const { id } = req.params;
-  const { rows } = await pool.query("select * from users where id = $1", [id]);
+router.get("/users/:id", userController.getUser);
 
-  if (rows.length === 0) {
-    return res.status(404).json({ message: "User not found" });
-  }
+router.post("/users", userDTO, validateFields, userController.createUser);
 
-  return res.json(rows[0]);
-});
+router.delete("/users/:id", userController.deleteUser);
 
-router.post("/users/:id", async (req, res) => {
-  const data = req.body;
-  const { rows } = await pool.query(
-    "insert into users(name, email) values($1, $2) RETURNING *",
-    [data.name, data.email]
-  );
-
-  return res.json(rows[0]);
-});
-
-router.delete("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const { rowCount } = pool.query("delete from users where id = $1", [id]);
-
-  if (rowCount === 0) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  return res.json({ message: "User deleted" });
-});
-
-router.put("/users/:id", async (req, res) => {
-  const { id } = req.params;
-  const data = req.body;
-
-  const { rows } = await pool.query(
-    "update users set name = $1, email = $2 where id = $3 RETURNING *",
-    [data.name, data.email, id]
-  );
-
-  return res.json(rows[0]);
-});
+router.put("/users/:id", userDTO, validateFields, userController.updateUser);
 
 export default router;
